49cfc77815e7e1188845b985d78489de8dd2aa04
Joel Gombin <joel.gombin@gmail.com>
2014-06-07 16:45:24 +0200

    documentation du package

---
+++ R/LireMinInterieur-package.R
@@ -0,0 +1,8 @@
+#' Ce package offre des outils pour nettoyer les fichiers électoraux du ministère de l'intérieur.
+#' @name LireMinInterieur
+#' @aliases LireMinInterieur-package
+#' @docType package
+#' @title Ce package offre des outils pour nettoyer les fichiers électoraux du ministère de l'intérieur.
+#' @author Joël Gombin \email{joel.gombin@@gmail.com}
+#' @keywords package
+NA
